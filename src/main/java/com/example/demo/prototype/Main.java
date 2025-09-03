04462f194d741301230f906a48e9b3e67d196cf7
oji-123 <om1997518@gmail.com>
2025-09-03 21:00:28 +0900

    Prototypeの作成

---
+++ src/main/java/com/example/demo/prototype/Main.java
@@ -0,0 +1,27 @@
+package com.example.demo.prototype;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// インスタンスの準備
+		Manager manager = new Manager();
+		UnderlinePen upen = new UnderlinePen('-');
+		MessageBox mbox = new MessageBox('*');
+		MessageBox sbox = new MessageBox('/');
+
+		// インスタンスの登録
+		manager.register("strong message", upen);
+		manager.register("warning box", mbox);
+		manager.register("srash box", sbox);
+
+		// インスタンスの取得と実行
+		Product p1 = manager.create("strong message");
+		p1.use("Hello World");
+
+		Product p2 = manager.create("warning box");
+		p2.use("Hello World");
+
+		Product p3 = manager.create("srash box");
+		p3.use("Hello World");
+	}
+}
---
+++ src/main/java/com/example/demo/prototype/Product.java
@@ -0,0 +1,7 @@
+package com.example.demo.prototype;
+
+public interface Product extends Cloneable {
+	public abstract void use(String s);
+
+	public abstract Product createCopy();
+}
